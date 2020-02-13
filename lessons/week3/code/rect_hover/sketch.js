4bcd70b05a4115e4483d6695c4456f28a10c135a
chandrunarayan <54154611+chandrunarayan@users.noreply.github.com>
2020-02-13 10:00:00 -0800

    button initial code

--- lessons/week3/code/rect_hover/sketch.js
+++ lessons/week3/code/rect_hover/sketch.js
@@ -4,12 +4,26 @@ function setup() {
 }
 
 function draw() {
-  background(204);
-  rect(100, 200, 180, 160);
-  
-  if (mouseX > 100 && mouseX < 280 && mouseY > 200 && mouseY < 360) {
-    fill(0);
+  background(220);
+  let s = 'Reset';  
+  if (mouseX > 100 && mouseX < 150 && mouseY > 200 && mouseY < 220) {
+    render_hovered_button(s, 100, 200)
+    // Some code here to react to isMousePressed to reset sketch variables
   } else {
+    render_dormant_button(s,100, 200)
+  } 
+}
+
+function render_hovered_button(s, x, y) {
+    fill(0);
+    rect(x, y, 50, 20);
+    fill(255)
+    text(s, x+7, y+3, 50, 20);
+}
+
+function render_dormant_button(s, x, y) {
     fill(255);
-  }
+    rect(x, y, 50, 20);
+    fill(0)
+    text(s, x+7, y+3, 50, 20);
 }
