fab93800924a852154d924e3211a43e5a29d8c34
cnarayan <cnarayan@cate.local>
2020-02-26 22:27:56 -0800

    Update sketch.js

--- lessons/week4/code/modifiedMovingBall/sketch.js
+++ lessons/week4/code/modifiedMovingBall/sketch.js
@@ -21,13 +21,13 @@ function draw() {
   
   if (keyIsPressed) {
     if (keyCode == UP_ARROW) {
-      y = max(rad, y - 10);
+      y = max(rad, y - 5);
     } else if (keyCode == DOWN_ARROW) {
-      y = min(y + 10, width-rad);
+      y = min(y + 5, width-rad);
     } else if (keyCode == RIGHT_ARROW) {
-      x = min(x + 10, width-rad);
+      x = min(x + 5, width-rad);
     } else if (keyCode == LEFT_ARROW) {
-      x = max(rad, x - 10);
+      x = max(rad, x - 5);
     }
   }
 }
