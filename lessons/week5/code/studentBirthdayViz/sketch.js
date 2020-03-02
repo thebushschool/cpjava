aa76fa7fcd030c668a4412df35063f9511d9a5ea
cnarayan <cnarayan@cate.local>
2020-03-01 21:38:21 -0800

    updates

--- lessons/week5/code/studentBirthdayViz/sketch.js
+++ lessons/week5/code/studentBirthdayViz/sketch.js
@@ -22,7 +22,7 @@ var rank = [364,362,356,350,338,301,324,347,351,349,341,306,316,260,304,322,337,
         7,5,3,6,11,27,21,23,22,19,20,53,88,89,79,83,84,97,119,122,160,136,180,116,109,155,216,239,219,159,135,
         164,212,269,209,184,172,142,173,252,355,233,222,178,131,143,207,236,241,192,182,158,194,312,263,238,
         190,161,148,147,151,199,340,344,352,353,357,358,359,345,231,149,144,186,283,320,318,284,221,217,205,
-        295,266,342,185,128,102,94,96,113,104,115,224,354,363,365,360,130,62,42,26,220];
+        295,266,342,185,128,102,94,96,113,104,115,224,354,363,365,360,130,62,42,26,220,67,120,200,100,35,130];
 
 var numColors = 160;
 var rectWidth = 40;
@@ -30,23 +30,46 @@ var rectHeight = 20;
 var gridOffset = 50;
 
 function setup(){
-
+  createCanvas(700, 800);  // sets the canvas size to 700 x 800
 }
 
 function draw(){
-
+  background(250, 182, 182);  // clears the background
+  drawGrid();                 // draws the gridlines
+  drawLabels();               // draws the graphs labels
+  displayValues();            // calls dispaly values, which will show values on hover
 }
 
 // Draws the day, and month labels
 // and the labels for the gradient key
 // as well as the gradient key itself
 function drawLabels() {
-
+  noStroke();
+  // day labels
+  textSize(11);
+  for (i = 0; i < 31; i++) {
+    text(i + 1, 65, 82 + i * 20); 
+  }
+  // month labels
+  text("   Jan        Feb      Mar       Apr        May     June      July       Aug      Sept      Oct       Nov      Dec", 90, 65);
+  
+  // labels for keys
+  text("Less Common", 165, 730);
+  text("More Common", 420, 730);
+  // gradient key
+  for(i = 0; i < numColors; i++) {
+    stroke(200 - i, 200 - i, 200 - i);
+    line(250 + i, 710, 250 + i, 740);
+  } 
 }
 
 // draws the grid of rectangles
 function drawGrid() {
-
+  noStroke();
+  for (i = 0; i <= 365; i++) {
+    fill(rank[i] * numColors / 366, rank[i] * numColors / 366, rank[i] * numColors / 366);
+    rect(gridOffset + getMonth(i) * rectWidth, gridOffset + getDay(i) * rectHeight, rectWidth - 2, rectHeight - 2);
+  }
 }
 
 // given the current index (out of 365)
@@ -82,18 +105,35 @@ function getDay(index) {
 // when the user hovers over a square
 // it displays the actual rank of that day
 function displayValues(){
-
+  // determine where the mouse is hovering
+  var monthIndex = (int)((mouseX - (gridOffset + rectWidth)) / rectWidth);   // 90 is the starting point of the left-most rects
+  var dayIndex = (int)((mouseY - (gridOffset + rectHeight)) / rectHeight);     // 70 is the starting points of the left-most rects
+  // if that mouse position is within the range of the data
+  if (monthIndex >= 0 && monthIndex < 12 && dayIndex >= 0 && dayIndex < getDaysInMonth(monthIndex)) {
+    // determine whether to show the text as black or white
+    // 183 is about when we should switch to black
+    if (rank[getNumDays(monthIndex) + dayIndex] > 183) {
+      fill(0); 
+    } else{
+      fill(255);
+    }
+    // display the actual rank
+    textSize(20);
+    text(rank[getNumDays(monthIndex) + dayIndex], mouseX, mouseY); 
+  }
 }
 
 // Given the current month (1-based)
 // returns the total number of days
 function getNumDays(monthIndex) {
-
+// ADD CODE HERE
+// RETURN SOMETHING HERE
 }
 // 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
 // takes in a month, zero based
 // and returns the days in that month
 // 0 is January, 11 is December
 function getDaysInMonth(month) {
-
+// ADD CODE HERE
+// RETURN SOMETHING HERE
 }
