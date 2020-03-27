9cb4a53c0f94b8b737965cda2d829f9ac30a93b7
cnarayan <cnarayan@cate.local>
2020-03-27 16:47:58 -0700

    buttons added

---
+++ misc_code/buttons/sketch.js
@@ -0,0 +1,56 @@
+/**
+ * changing size and position of circles in a grid
+ *
+ * MOUSE
+ * position x          : circle position
+ * position y          : circle size
+ * left click          : random position
+ *
+ * KEYS
+ * s                   : save png
+ */
+'use strict';
+
+var tileCount = 20;
+var actRandomSeed = 0;
+
+var circleAlpha = 130;
+var circleColor;
+
+function setup() {
+  createCanvas(600, 600);
+  noFill();
+  circleColor = color(0, 0, 0, circleAlpha);
+}
+
+function draw() {
+  translate(width / tileCount / 2, height / tileCount / 2);
+  background(255);
+
+  randomSeed(actRandomSeed);
+
+  stroke(circleColor);
+  strokeWeight(mouseY / 60);
+
+  for (var gridY = 0; gridY < tileCount; gridY++) {
+    for (var gridX = 0; gridX < tileCount; gridX++) {
+
+      var posX = width / tileCount * gridX;
+      var posY = height / tileCount * gridY;
+
+      var shiftX = random(-mouseX, mouseX) / 20;
+      var shiftY = random(-mouseX, mouseX) / 20;
+
+      ellipse(posX + shiftX, posY + shiftY, mouseY / 15, mouseY / 15);
+      //ellipse(posX, posY, mouseY / 15, mouseY / 15);
+    }
+  }
+}
+
+function mousePressed() {
+  actRandomSeed = random(100000);
+}
+
+function keyReleased() {
+  if (key == 's' || key == 'S') saveCanvas('mycanvas_' + str(millis()), 'png');
+}
