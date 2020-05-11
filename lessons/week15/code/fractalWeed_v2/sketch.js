1cb2bc6cb0365eac4c6b0788a67e4dc8b2357695
cnarayan <cnarayan@cate.local>
2020-05-11 10:31:34 -0700

    updates wk15

---
+++ lessons/week15/code/fractalWeed_v2/sketch.js
@@ -0,0 +1,70 @@
+var angle;
+var axiom = 'X';
+var sentence = axiom;
+var len = 10;
+
+var rules = [];
+rules[0] = {
+  a: 'X',
+  b: 'F+[[X]-X]-F[-FX]+X)',
+  c: 'FF'  
+};
+rules[1] = {
+  a: 'F',
+  b: 'FF'
+};
+
+function generate() {
+  len *= 0.86;
+  var nextSentence = '';
+  for (var i = 0; i < sentence.length; i++) {
+    var current = sentence.charAt(i);
+    var found = false;
+    for (var j = 0; j < rules.length; j++) {
+      if (current == rules[j].a) {
+        found = true;
+        nextSentence += rules[j].b;
+        break;
+      }
+    }
+    if (!found) {
+      nextSentence += current;
+    }
+  }
+  sentence = nextSentence;
+  createP(sentence);
+  turtle();
+}
+
+function turtle() {
+  background(51);
+  resetMatrix();
+  translate(width / 3, height - 10);
+  stroke(255, 255);
+  for (var i = 0; i < sentence.length; i++) {
+    var current = sentence.charAt(i);
+
+    if (current == 'F') {
+      line(0, 0, 0, -len);
+      translate(0, -len);
+    } else if (current == '-') {
+      rotate(angle);
+    } else if (current == '+') {
+      rotate(-angle);
+    } else if (current == '[') {
+      push();
+    } else if (current == ']') {
+      pop();
+    }
+  }
+}
+
+function setup() {
+  createCanvas(400, 800);
+  angle = -25;
+  background(51);
+  createP(axiom);
+  turtle();
+  var button = createButton('generate');
+  button.mousePressed(generate);
+}
--- lessons/week15/plan/inspiration.md
+++ lessons/week15/plan/inspiration.md
@@ -3,7 +3,14 @@
 ## Week 15 - 5/11
 
 ### Inspiration for Final Project IDEAS
+
+#### Objects and Array Lists
 * [Jitter Bug Exercise Example 1](../code/jitterBug_v2)
 * [Bubbles Exercise Example 2](../code/arrayBubbles_v6)
 * [Planets Exercise Example 3](../code/planets_v1)
-* [Perlin Noise](../code/p5)
+
+#### Perlin Noise
+* [Perlin Noise](../code/pn5)
+
+#### Recursion and L-Systems
+* [Fractal Weed](../code/fractalWeed_v2)
